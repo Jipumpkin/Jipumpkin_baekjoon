08ebba658821a5170e5ad8cffe8cd4f68427bdc3
Jipumpkin <61648483+Jipumpkin@users.noreply.github.com>
2024-07-13 12:56:52 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 168 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -8,13 +8,16 @@ public class Main {
         String word = sc.nextLine();
         int Wcount = word.length();
 
-        
+        // 크로아티아 알파벳 배열
         String[] croa = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
-        
+        // 크로아티아 알파벳 개수를 카운트할 변수
         int croCount = 0;
 
-        
+        // 입력된 문자열을 순회하며 크로아티아 알파벳을 찾아내고
+        // 공백으로 대체함
+        // ""로 대체하지 않은 이유는 문자 사이를 일부러 구분하여서
+        // 중복패턴이 발생하는것을 막기 위함
         for (String cro : croa) {
             while (word.contains(cro)) {
                 word = word.replaceFirst(cro, " ");
@@ -22,7 +25,7 @@ public class Main {
             }
         }
 
-        
+        // 남은 문자중 공백을 제거하고 단일 알파벳으로 취급하여 갯수를 셈
         word = word.replaceAll(" ", "");
         Wcount = croCount + word.length();
 
